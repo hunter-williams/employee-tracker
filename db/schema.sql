d87dbf640a37dd925944a5baa3d3294c29a987f6
hunter-williams <dragynson@yahoo.com>
2022-10-31 18:18:18 -0700

    created db schema

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_title VARCHAR(30) NOT NULL,
+    role_salary DECIMAL(10,2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE  
+)
+    
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+   ) 
+
+   -- Primary key is id
+   -- foreign key is id from another table
+   -- refrences is what table to foreign key comes from
+
+   -- on delete cascade is delete child refrence when parent is deleted // if i delete a role, remove id from employee
+   -- on delete set null is null child refrence when parent is deleted
